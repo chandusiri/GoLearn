d4f8935f4932b3a70c275ced3812ec685c33f877
chandusiri <chandu.siri14@gmail.com>
2020-08-18 20:22:55 +0530

    See TYPE, var, zero value, packages, own types and conversion in Go

--- basics/the_var_keyword.go
+++ basics/the_var_keyword.go
@@ -7,6 +7,9 @@ import "fmt"
 // Scope of this variable would be of package level
 var y = 43
 
+// Declare two variables of same TYPE at a time
+var i, j int
+
 // Assigns "The zero value" of TYPE int to "z" by default
 // The zero value is false for booleans, 0 for integers, 0.0 for floats, "" for strings,
 // and nil for pointers, functions, interfaces, slices, channels, and maps.
@@ -17,10 +20,16 @@ func main() {
 	// scope is statements from declaration till end of function body
 	// Best practice is to use variables with lesser scope
 	x := 42
+	i, j = 45, 55
+	// Swapping
+	i, j = j, i
+	fmt.Println(i, j)
+
 	fmt.Println(x)
 	fmt.Println(y)
 	foo()
 	fmt.Println(z)
+
 }
 
 func foo() {
